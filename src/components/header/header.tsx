5a89be2619cbf1f5a79c74abb3923fb5efe04e7c
Abdulla <dream05@mail.ru>
2024-12-07 15:49:10 +0500

    added change theme

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import { FC, useRef } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import { images } from "../../assets/image";
 import { useAppDispatch } from "../../store/hooks";
 import { dataCity } from "../../store/weather";
@@ -14,6 +14,29 @@ const Header: FC = () => {
       inputRef.current.value = ''
     }
   }
+  const [theme, setTheme] = useState('light');
+    const changeTheme = ()=>{
+        if (theme == 'light') {
+            setTheme('dark')
+        } else {
+            setTheme('light')            
+        }
+    }
+
+    useEffect(()=>{
+        const localTheme = localStorage.getItem('theme');
+        if(localTheme)
+        setTheme(localTheme)
+    }, [])
+
+    useEffect(()=>{
+        localStorage.setItem('theme', theme)
+        if (theme == 'light') {
+            document.body.classList.remove('active');
+        } else {
+            document.body.classList.add('active');            
+        }
+    }, [theme])
   return (
     <nav className="nav">
       <a href="" className="logo">
@@ -21,7 +44,7 @@ const Header: FC = () => {
         vue weather
       </a>
       <div className="search">
-        <img src={images.city} alt="" className="search__icon" />
+        <img src={images.city} alt="" className="search__icon" onClick={changeTheme} />
         <input type="text" className="search__input" placeholder="Выбрать город" ref={inputRef} onKeyDown={setCityListener} />
       </div>
     </nav>
--- src/services/weather-service.ts
+++ src/services/weather-service.ts
@@ -65,7 +65,7 @@ class WeatherService {
        throw new Error(responseWeather.statusText);
       }
       const weather: IFullWeather = responseWeather.data;
-      console.log(weather);
+      // console.log(weather);
       return { ...weather, name: local_names.ru }
     } catch (error) {
       throw error;
